012de00dc870c2b30615aed1ee39479c954f4118
omrimusayov <omrimuss@gmail.com>
2026-09-08 20:12:24 +0300

    pr

---
+++ Project-RED/Models/student.cs
@@ -0,0 +1,11 @@
+﻿namespace project_red.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GradeLevel { get; set; }
+
+    }
+}
